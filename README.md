aa14526a779a7e543ace824ab79e3f218cab829f
Aditya <adityakul14@gmail.com>
2024-10-23 12:38:54 +0530

    updated

---
+++ README.md
@@ -0,0 +1,2 @@
+# Job-portal
+job portal project ,In that search job as per our intrest and apply for job 
---
+++ src/App.js
@@ -0,0 +1,28 @@
+import {BrowserRouter  as Router, Routes, Route} from 'react-router-dom';
+import Home from './Components/Home';
+import './App.css';
+import TopRated from './Components/TopRated';
+import UpcomingMovies from './Components/UpcomingMovies';
+import SingleMovie from  './Components/SingleMovie';
+import Search from './Components/Search';
+
+
+
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/top" element={<TopRated />} />
+          <Route path="/upcoming" element={<UpcomingMovies />} />
+          <Route path="/onemovie/:id" element={<SingleMovie />} />
+          <Route path="/search/:query" element={<Search />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
